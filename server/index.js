34850f105f390d51354845553960f54a6ce7af23
Stefano Valle <s.valle@mvassociati.it>
2018-11-08 05:59:48 +0100

    Adapt JS server to cars example

--- server/index.js
+++ server/index.js
@@ -3,18 +3,18 @@ var restify = require('restify');
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
-server.get('/stations', function(req, res, next) {
-    res.send('List of stations: [TODO]');
+server.get('/cars', function(req, res, next) {
+    res.send('List of cars: [TODO]');
     return next();
 });
 
-server.get('/stations/:station', function(req, res, next) {
-    res.send('Current values for station ' + req.params['station'] + ': [TODO]');
+server.get('/cars/:plate', function(req, res, next) {
+    res.send('Current values for car ' + req.params['plate'] + ': [TODO]');
     return next();
 });
 
-server.post('/stations/:station', function(req, res, next) {
-    res.send('Data received from station [TODO]');
+server.post('/cars/:plate', function(req, res, next) {
+    res.send('Data received from plate [TODO]');
 
     // uncomment to see posted data
     //console.log(req.body);
