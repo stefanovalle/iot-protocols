d837fe8a1db67e7f8bb86e59bf4c27bf9e6df0c7
Stefano Valle <s.valle@mvlabs.it>
2020-12-17 06:20:26 +0100

    Refactor client code

--- client/Client/Program.cs
+++ client/Client/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Client.Sensors;
-using System.Net;
-using System.IO;
-using System.Collections;
+using Client.Protocols;
 
 namespace Client
 {
@@ -16,24 +14,15 @@ namespace Client
             List<SensorInterface> sensors = new List<SensorInterface>();
             sensors.Add(new VirtualSpeedSensor());
 
+            // define protocol
+            ProtocolInterface protocol = new Http("http://localhost:8011/scooters/123");
+
+            // send data to server
             while (true)
             {
                 foreach (SensorInterface sensor in sensors)
                 {
-                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:8011/scooters/123");
-                    httpWebRequest.ContentType = "text/json";
-                    httpWebRequest.Method = "POST";
-
-                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                    {
-                        streamWriter.Write(sensor.toJson());
-                    }
-
-                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-                    Console.Out.WriteLine(httpResponse.StatusCode);
-
-                    httpResponse.Close();
+                    protocol.Send(sensor.toJson());
 
                     System.Threading.Thread.Sleep(1000);
 
