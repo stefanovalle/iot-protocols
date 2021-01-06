e49486bcd25aa21d87cf522abda250eb0763859f
Stefano Valle <s.valle@mvlabs.it>
2021-01-06 07:28:00 +0100

    Client: add support for MQTT

--- client/Client/Program.cs
+++ client/Client/Program.cs
@@ -15,7 +15,13 @@ namespace Client
             sensors.Add(new VirtualSpeedSensor());
 
             // define protocol
-            ProtocolInterface protocol = new Http("http://localhost:8011/scooters/123");
+            //ProtocolInterface protocol = new Http("http://localhost:8011/scooters/123");
+            
+            ProtocolInterface protocol = new Mqtt("localhost", "monopattino/speed");
+            /* refactoring necessari alla riga precedente:
+             * - spostare secondo parametro su metodo "Send" (che fra l'altro dipende da *cosa* si sta inviando), modificando l'interfaccia per adattarla anche ad altri protocolli
+             * - usare stringhe migliori come topic (vedi suggerimenti a lezione)
+             */
 
             // send data to server
             while (true)
