93c65998ee7c7b83a7408a3c549f3a064ada78f7
Stefano Valle <s.valle@mvlabs.it>
2021-01-20 12:32:50 +0100

    Add AMQP support

--- client/Client/Program.cs
+++ client/Client/Program.cs
@@ -16,11 +16,11 @@ namespace Client
 
             // define protocol
             //ProtocolInterface protocol = new Http("http://localhost:8011/scooters/123");
-            
-            ProtocolInterface protocol = new Mqtt("localhost", "monopattino/speed");
+            //ProtocolInterface protocol = new Mqtt("localhost", "monopattino/speed");
+
+            ProtocolInterface protocol = new Amqp("crow.rmq.cloudamqp.com", "monopattino");
             /* refactoring necessari alla riga precedente:
              * - spostare secondo parametro su metodo "Send" (che fra l'altro dipende da *cosa* si sta inviando), modificando l'interfaccia per adattarla anche ad altri protocolli
-             * - usare stringhe migliori come topic (vedi suggerimenti a lezione)
              */
 
             // send data to server
