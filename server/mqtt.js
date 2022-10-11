78d5616ac93ede265e9b737a75903bc586cf6b00
Stefano Valle <s.valle@mvlabs.it>
2022-10-11 15:59:44 +0200

    Update mqtt listener and files

---
+++ server/mqtt.js
@@ -0,0 +1,17 @@
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+    console.log("Connesso");
+    /*client.subscribe('presence', function (err) {
+        if (!err) {
+        client.publish('presence', 'Hello mqtt')
+        }
+    })*/
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
